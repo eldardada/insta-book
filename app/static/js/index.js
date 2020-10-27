bebf5fbff4f833e92d2ff547252e0dc4d33872ef
eldardada <eldar-dadashov@mail.ru>
2020-10-27 10:26:30 +0300

    Сверстал секцию feedbacks для ПК

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,4 +1,71 @@
 // import 'core-js/features/promise';
-import avg from './modules/some.js';
+import Swiper, {Navigation, Thumbs, EffectFade} from 'swiper'; 
 
-console.log(avg(1,2,3));
+Swiper.use([Navigation, Thumbs, EffectFade]);
+
+
+const galleryThumbs = new Swiper('.feedback__gallery-thumbs', {
+    allowTouchMove: false,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+
+const galleryTop = new Swiper('.feedback__gallery-top', {
+    allowTouchMove: false,
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+});
+
+const feedbackContentSlider = new Swiper('.feedback__gallery-content', {
+    allowTouchMove: false,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    }
+});
+
+
+
+let thumbsImages = document.querySelectorAll('.feedback__gallery-thumbs .swiper-slide');
+let galetyImages = document.querySelectorAll('.feedback__gallery-top .swiper-slide');
+
+const thumbContainer = document.querySelector('.feedback__gallery-thumbs');
+
+function thumbsImagesRemoveActive() {
+    thumbsImages.forEach(el => {
+        if(el.classList.contains('swiper-slide-active')) {
+            el.classList.remove('swiper-slide-active');
+        }
+    });
+}
+
+
+galleryTop.on('slideChange', (e) => {
+    let index = e.activeIndex - 1;
+    index === 0 ? index = thumbsImages.length : 1;
+    index === thumbsImages.length ? index = 0 : 1;
+    feedbackContentSlider.forEach(el => {
+        el.slideTo(index);
+    });
+    
+    thumbsImagesRemoveActive();
+    console.log(index);
+
+    !thumbsImages[index] ? index = 0 : 1;
+    console.log(index);
+    thumbsImages[index].classList.add('swiper-slide-active');
+});
+
