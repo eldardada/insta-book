44cc22ef0fdc82e2f133c5b64b53058f43c8aff3
eldardada <eldar-dadashov@mail.ru>
2020-10-28 18:18:32 +0300

    Улучшена версия для мобилок, в частности слайдер стал более адаптивным

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -4,32 +4,26 @@ Swiper.use([Navigation, Thumbs, EffectFade]);
 
 window.addEventListener('load', function() {
     // import 'core-js/features/promise';
-    
 
     const galleryThumbs = new Swiper('.feedback__gallery-thumbs', {
-        slidesPerView: 2,
+        width: 68,
+        allowTouchMove: true,
+        slidesPerView: 1,
         spaceBetween: 20,
-        freeMode: true,
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
-        allowTouchMove: false,
+        freeMode: true,
         breakpoints: {
-            341: {
-                slidesPerView: 3,
-            },
-            576: {
-                slidesPerView: 5,
-            },
             901: {
-                slidesPerView: 7,
-                
+                watchSlidesVisibility: false,
+                watchSlidesProgress: false,
+                allowTouchMove: false 
             }
         }
     });
     
     const galleryTop = new Swiper('.feedback__gallery-top', {
-        slidesPerView: 4,
-        loop: true,
+        slidesPerView: 1,
         effect: 'fade',
         fadeEffect: {
             crossFade: true
@@ -53,35 +47,18 @@ window.addEventListener('load', function() {
         
     });
     
-    let thumbsImages = document.querySelectorAll('.feedback__gallery-thumbs .swiper-slide');
-    let galetyImages = document.querySelectorAll('.feedback__gallery-top .swiper-slide');
     
-    const thumbContainer = document.querySelector('.feedback__gallery-thumbs');
+    galleryTop.on('slideChange', e => {
+        let index = e.activeIndex;
     
-    function thumbsImagesRemoveActive() {
-        thumbsImages.forEach(el => {
-            if(el.classList.contains('swiper-slide-active')) {
-                el.classList.remove('swiper-slide-active');
-            }
-        });
-    }
-    
-    
-    galleryTop.on('slideChange', (e) => {
-        let index = e.activeIndex - 1;
-        
-        index === 0 ? index = thumbsImages.length : 1;
-        index === thumbsImages.length ? index = 0 : 1;
-        
         feedbackContentSlider.forEach(el => {
             el.slideTo(index);
         });
         
-        thumbsImagesRemoveActive();
-    
-        !thumbsImages[index] ? index = 0 : 1;
-    
-        thumbsImages[index].classList.add('swiper-slide-active');
+    });
+
+    galleryThumbs.on('slideChange', function(e) {
+        if(galleryThumbs.isEnd) console.log(1);
     });
     
 });
